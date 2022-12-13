acc0ddbcb2fa7ac6f5a279bf42b68257aa7c07be
Bapi Biswas <star.bappy@gmail.com>
2022-12-13 13:04:54 +0600

    homework completed

---
+++ script.js
@@ -0,0 +1,118 @@
+// let firstName = "asif";
+// let age = 33;
+// console.log(firstName);
+// console.log(age);
+
+/*
+Type of Data
+1.Object
+2.Primitive
+
+// *primitive data type
+// 1.Number
+// 2.string
+// 3.boolean
+// -------------rare use
+// 4.Undefined
+// 5.Null
+// 6.symbol
+// 7.bigInt
+// */
+// // number
+// let age = 28;
+// console.log(age);
+// // String
+// let firstName = "asif";
+// console.log(firstName);
+// // Boolean
+// let isSinger = true;
+// console.log(isSinger);
+// // undefined
+// let homeCity;
+// console.log(homeCity);
+// // Null
+// let income = null;
+// console.log(income);
+
+// console.log(typeof age);
+// console.log(typeof homeCity);
+// console.log(typeof isSinger);
+// console.log(typeof income); //JS bug
+// homeCity = "Khulna";
+// console.log(homeCity);
+// isSinger = "Bangladeshi Singer";
+// console.log(isSinger);
+// console.log(typeof isSinger);
+// Type of Variable
+/*
+1.let // Changable 
+2.const //not changable constant
+3.var  //same as let, we wont use it.
+*/
+// let myAge = 31;
+// console.log(myAge);
+// myAge = 35;
+// console.log(myAge);
+// const birthYear = 1991;
+// console.log(birthYear);
+// var job = "teacher";
+// console.log(job);
+
+// javascript operators Basic
+console.log(18 - 12);
+let profit = 32 - 28;
+console.log(profit);
+let currentYear = 2022;
+let myAge = currentYear - 1991;
+console.log(myAge);
+let bishuAge = currentYear - 2002;
+console.log(bishuAge);
+let ageDiff = myAge - bishuAge;
+console.log(ageDiff);
+
+// Arithmatic Operator
+/*
++, -, *, /, **, %, ++, --
+*/
+
+// console.log(10 + 5);
+// console.log(20 - 5);
+// console.log(5 * 5);
+// console.log(30 / 5);
+// console.log(2 ** 4);
+// console.log(21 % 4);
+// let x = 10;
+// x++;
+// console.log(x);
+// let y = 20;
+// y--;
+// console.log(y);
+// console.log(100 - 50 + 20 * 2 + 20 / 4);
+
+// let firstName = "asif";
+// let lastName = "akbar";
+// console.log(firstName + " " + lastName);
+
+// Assignment operator
+/*
+= , += , -= , *= , /=
+*/
+let item = 10 + 5;
+console.log(item);
+
+let x = 15;
+x += 10;
+console.log(x);
+// x=x+10
+let y = 20;
+y -= 15;
+console.log(y);
+// y=y-15
+let z = 5;
+z *= 5;
+console.log(z);
+// z=z*5
+let w = 15;
+w /= 3;
+console.log(w);
+// w=w/3
